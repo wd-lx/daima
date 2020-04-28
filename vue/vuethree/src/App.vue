17e5004e7ea61bdcacf8a5cef4cf5cb3dc520f77
wangdan <1642032381@qq.com>
2020-04-28 23:39:19 +0800

    路由

---
+++ vue/vuethree/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <a href="#/">首页</a>
+    <a href="#/page">page页面</a> -->
+    <!-- <a href="/">首页</a>
+    <a href="/page">page页面</a> -->
+    <router-link to='/'>首页</router-link>
+    <router-link to='/page'>page页面</router-link>
+    <router-link to='/work'>work页面</router-link>
+    <router-link to='/first'>first页面</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+.router-link-active{
+  color: rgb(32, 4, 4);
+}
+</style>
---
+++ vue/vuethree/src/router/index.js
@@ -0,0 +1,80 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import page from '@/components/page'
+import work from '@/components/work'
+import first from '@/components/first'
+const User = {
+    template: `<div>
+  <h3><font color="green">我是父组件 {{$route.params.id}} </font></h3>
+  <router-view/>
+  </div>`
+}
+const Profile = {
+    template: `<div><h3><font color="red">我是Profile子组件 </font></h3></div>`
+}
+const Posts = {
+    template: `<div><h3><font color="green">我是posts组件 </font></h3></div>`
+}
+const NotFount = {
+    template: `<div><h3>  404您访问的页面不存在 </h3></div>`
+}
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            redirect: '/page'
+                // name: 'HelloWorld',
+                // component: HelloWorld
+        },
+        {
+            path: '/page',
+            name: "page",
+            component: page
+        },
+        {
+            path: '/work',
+            name: "work",
+            component: work
+        },
+        {
+            path: '/first/:id',
+            name: "first",
+            component: first
+        },
+        {
+            path: '/user/:id',
+            name: "User",
+            component: User,
+            children: [{
+                    path: 'profile',
+                    name: "Profile",
+                    component: Profile,
+                },
+                {
+                    path: '/posts',
+                    name: "Posts",
+                    component: Posts,
+                }
+            ]
+        },
+        // 路由的重定向：表示将你原来在转发列表中发向路由的路径改成另一条路由的路径，
+        // 你的数据会存到另一路由的服务器上
+        // {
+        //     path: "*",
+        //     // component: NotFount,
+        //     redirect: (to) => {
+        //         console.log(to);
+        //         if (to.path === "/aaa") {
+        //             return "/page"
+        //         } else if (to.path === "/bbb") {
+        //             return "/work"
+        //         } else {
+        //             return '/'
+        //         }
+        //     }
+        // }
+    ]
+})
